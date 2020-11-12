7fb68c191d25fcd0bcf696097d3b8ffab6753957
LiLei <tobiaslee@ralee.cc>
2020-11-12 20:41:52 +0800

    fix nested ner bug and preprocess

--- cluener_bios/preprocess.py
+++ cluener_bios/preprocess.py
@@ -1,4 +1,7 @@
-import json 
+import json
+
+
+
 
 def read_json(input_file):
     lines = []
@@ -10,24 +13,40 @@ def read_json(input_file):
             words = list(text)
             labels = ['O'] * len(words)
             if label_entities is not None:
+                all_spans = []
                 for key, value in label_entities.items():
                     for sub_name, sub_index in value.items():
                         for start_index, end_index in sub_index:
-                            assert ''.join(words[start_index:end_index + 1]) == sub_name
-                            if start_index == end_index:
-                                labels[start_index] = 'S-' + key
-                            else:
-                                labels[start_index] = 'B-' + key
-                                labels[start_index + 1:end_index + 1] = ['I-' + key] * (len(sub_name) - 1)
+                            all_spans.append((key, sub_name, start_index, end_index ))
+
+                all_spans = sorted(all_spans, key=lambda x: x[3] - x[2])
+                for key, sub_name, start_index, end_index in all_spans:
+                    assert ''.join(words[start_index:end_index + 1]) == sub_name
+                    if start_index == end_index:
+                        labels[start_index] = 'S-' + key
+                    else:
+                        labels[start_index] = 'B-' + key
+                        labels[start_index + 1:end_index + 1] = ['I-' + key] * (len(sub_name) - 1)
             lines.append({"words": words, "labels": labels})
+            #         for sub_name, sub_index in value.items():
+            #             for start_index, end_index in sub_index:
+            #                 assert ''.join(words[start_index:end_index + 1]) == sub_name
+            #                 if start_index == end_index:
+            #                     labels[start_index] = 'S-' + key
+            #                 else:
+            #                     labels[start_index] = 'B-' + key
+            #                     labels[start_index + 1:end_index + 1] = ['I-' + key] * (len(sub_name) - 1)
+            # lines.append({"words": words, "labels": labels})
     return lines
 
-files = [  k + '.json' for k in ['dev', 'test', 'train']]
+
+files = [k + '.json' for k in ['dev', 'test', 'train']]
+# files = ['train.json']
 for f in files:
     lines = read_json(f)
     for l in lines:
         w, l = l['words'], l['labels']
-        with open(f + ".txt", "a") as fw:
+        with open(f.replace(".json", ".txt"), "a") as fw:
             for tok, lbl in zip(w, l):
                 fw.write(tok + " " + lbl + "\n")
             fw.write("\n")
