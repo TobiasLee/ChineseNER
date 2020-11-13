2717a5f5048503cfc8bade91053f1abf91fb2520
LiLei <tobiaslee@ralee.cc>
2020-11-13 10:32:19 +0800

    update train.txt with cleaned data

--- cluener_bios/preprocess.py
+++ cluener_bios/preprocess.py
@@ -1,8 +1,6 @@
 import json
 
 
-
-
 def read_json(input_file):
     lines = []
     with open(input_file, 'r') as f:
@@ -17,17 +15,22 @@ def read_json(input_file):
                 for key, value in label_entities.items():
                     for sub_name, sub_index in value.items():
                         for start_index, end_index in sub_index:
-                            all_spans.append((key, sub_name, start_index, end_index ))
+                            all_spans.append((key, sub_name, start_index, end_index))
 
                 all_spans = sorted(all_spans, key=lambda x: x[3] - x[2])
+                # print(all_spans)
                 for key, sub_name, start_index, end_index in all_spans:
                     assert ''.join(words[start_index:end_index + 1]) == sub_name
+
                     if start_index == end_index:
                         labels[start_index] = 'S-' + key
                     else:
                         labels[start_index] = 'B-' + key
                         labels[start_index + 1:end_index + 1] = ['I-' + key] * (len(sub_name) - 1)
-            lines.append({"words": words, "labels": labels})
+                    # if sub_name == '《招商银行：投资永隆银行浮亏逾百亿港元》':
+                    # print(labels)
+                    # assert 1 == 0
+                lines.append({"words": words, "labels": labels})
             #         for sub_name, sub_index in value.items():
             #             for start_index, end_index in sub_index:
             #                 assert ''.join(words[start_index:end_index + 1]) == sub_name
